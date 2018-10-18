cc65904eeaaeff45a662123627d000ca5bdd5386
Brent Woodward <Brent.Woodward.1@gmail.com>
2018-10-17 17:16:21 -0700

    store form functioning, columns totallying

--- js/app.js
+++ js/app.js
@@ -1,22 +1,12 @@
 'use strict';
 
-//establish global variables
+//+++++++++++++++++++++ DATA +++++++++++++++++++++++++++
 var hoursOfOperation = ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'];
 var allStoreLocations = [];
 var storeCookiesSalesTable = document.getElementById('store-sales-projection-table');
+var newStoreForm = document.getElementById('new-store-form');
 
-// math equation for inclusive random number within a range of customers
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min; // from MDN docs
-}
 
-function newElement(type, content, parent) {
-  var element = document.createElement(type);
-  element.textContent = content;
-  parent.appendChild(element);
-}
-
-// build constructor function
 function SalmonStand(storeLocation, minCustomerPerHour, maxCustomerPerHour, avgCookiesSoldPerCustomer) {
   this.storeLocation = storeLocation;
   this.minCustomerPerHour = minCustomerPerHour;
@@ -25,10 +15,10 @@ function SalmonStand(storeLocation, minCustomerPerHour, maxCustomerPerHour, avgC
   this.avgCustomerPerHour = [];
   this.cookiesSoldPerHour = [];
   this.dailyTotal = 0;
+
   allStoreLocations.push(this);
 }
 
-//prototype for calc avg customer per hour
 SalmonStand.prototype.calcAvgCustomerPerHour = function () {
   for(var i = 0; i < hoursOfOperation.length; i++) {    
     var randomCustomer = random(this.minCustomerPerHour, this.maxCustomerPerHour);
@@ -37,7 +27,6 @@ SalmonStand.prototype.calcAvgCustomerPerHour = function () {
   }
 }
 
-//prototype for calc cookies per hour
 SalmonStand.prototype.calcAvgCookiesPerHour = function() {
   this.calcAvgCustomerPerHour();
   for(var i = 0; i < hoursOfOperation.length; i++) {
@@ -47,29 +36,6 @@ SalmonStand.prototype.calcAvgCookiesPerHour = function() {
     this.dailyTotal += hourlyCookies;
   }
 }
-
-// SalmonStand.prototype.calcHourlyTotals = function() {
-//   //variable for totals reset to 0
-//   //loop to cycle through stores
-//   //loop to cycle through hours
-//   var totalPerHour = 0
-//   for(var i = 0; i < hoursOfOperation.length; i++) {
-
-//     for(var j = 0; allStoreLocations.length; i++) {
-//       totalPerHour += 
-//     }
-//   }
-// }
-
-
-//use constructor to instantiate stores
-
-new SalmonStand('1st and Pike', 23, 65, 6.3);
-new SalmonStand('SeaTac', 3, 24, 1.2);
-new SalmonStand('Seattle Center', 11, 38, 3.7);
-new SalmonStand('Capitol Hill', 20, 38, 2.3);
-new SalmonStand('Alki', 2, 16, 4.6);
-
 
 SalmonStand.prototype.render = function () {
   this.calcAvgCookiesPerHour();
@@ -85,7 +51,24 @@ SalmonStand.prototype.render = function () {
   storeCookiesSalesTable.appendChild(trEl);
 }
 
-// can these be self calling?
+new SalmonStand('1st and Pike', 23, 65, 6.3);
+new SalmonStand('SeaTac', 3, 24, 1.2);
+new SalmonStand('Seattle Center', 11, 38, 3.7);
+new SalmonStand('Capitol Hill', 20, 38, 2.3);
+new SalmonStand('Alki', 2, 16, 4.6);
+
+
+//+++++++++++++++++++++ Function Declarations +++++++++++++++++++++++++++
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; // from MDN docs
+}
+
+function newElement(type, content, parent) {
+  var element = document.createElement(type);
+  element.textContent = content;
+  parent.appendChild(element);
+}
+
 function makeHeaderRow() {
   var trEl = document.createElement('tr');
   newElement('th', 'Store Location', trEl);
@@ -98,11 +81,58 @@ function makeHeaderRow() {
   newElement('th', 'Total', trEl);
 }
 
+function makeFooterRow() {
+  var trEl = document.createElement('tr');
+  var hourlyTotals = [];
+
+  // self-invoking function to calculate footer row totals
+  for(var i = 0; i < hoursOfOperation.length; i++) {
+    var total = 0;
+
+    for(var j = 0; j < allStoreLocations.length; j++) {
+      total += allStoreLocations[j].cookiesSoldPerHour[i];
+    }
+    hourlyTotals.push(total);
+  }
+    
+  newElement('td', 'Hourly Total', trEl);
+
+  for (var i = 0; i< hoursOfOperation.length; i++) {
+    newElement('td', hourlyTotals[i], trEl);
+    storeCookiesSalesTable.appendChild(trEl);
+  }
+
+  newElement('th', 'Total', trEl);
+}
+
 function renderAllStores() {
   for (var i = 0; i < allStoreLocations.length; i++) {
     allStoreLocations[i].render();
   }
 }
 
+function handleStoreLocationSubmit(event) {
+  event.preventDefault();
+
+  var location = event.target.storeLocation.value;
+  var minCust = event.target.minCustomers.value;
+  var maxCust = event.target.maxCustomers.value;
+  var avgSale = event.target.avgCookieSale.value;
+
+  new SalmonStand(location, minCust, maxCust, avgSale);
+
+  event.target.storeLocation.value = null;
+  event.target.minCustomers.value = null;
+  event.target.maxCustomers.value = null;
+  event.target.avgCookieSale.value = null;
+
+  allStoreLocations.slice(-1)[0].render();
+  makeFooterRow();
+}
+//+++++++++++++++++++++ Exicutables +++++++++++++++++++++++++++
+newStoreForm.addEventListener('submit', handleStoreLocationSubmit);
+
 makeHeaderRow();
 renderAllStores();
+makeFooterRow();
+//+++++++++++++++++++++ WIP +++++++++++++++++++++++++
